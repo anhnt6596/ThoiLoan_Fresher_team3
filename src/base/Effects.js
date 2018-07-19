4fb313e94c67dca7be14453cec81b9e2ea45ef28
CPU02417_LOCAL <anhnt6596@gmail.com>
2018-07-19 16:53:47 +0700

    giao dien

--- src/base/Effects.js
+++ src/base/Effects.js
@@ -1,5 +1,5 @@
 var ui = ui || {};
-ui.makeAnimation =  function(name, s, e, fps){
+ui.makeAnimation = function(name, s, e, fps){
     var i;
     var animFrames = [];
     for(i = s; i <= e; ++i){
@@ -9,4 +9,20 @@ ui.makeAnimation =  function(name, s, e, fps){
 
     var animation = new cc.Animation(animFrames, fps);
     return cc.animate(animation);
+};
+
+ui.BounceEff = function() {
+    var act1 = new cc.ScaleTo(0.15, 1.1, 1.1);
+    var act2 = new cc.ScaleTo(0.05, 1, 1);
+    return new cc.Sequence(act1, act2);
+};
+
+ui.targettingEff = function() {
+    var act1 = cc.tintTo(0.75, 220, 200, 220);
+    var act2 = cc.tintTo(0.75, 255, 255, 255);
+    return new cc.Sequence(act1, cc.delayTime(0.25), act2, cc.delayTime(0.25));
+};
+
+ui.backToDefaultColor = function() {
+    return cc.tintTo(0, 255, 255, 255);
 };
