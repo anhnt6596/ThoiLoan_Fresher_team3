6fbb7a9334368ea8d46112b8643517fdd42c5b9a
CPU02417_LOCAL <anhnt6596@gmail.com>
2018-07-25 15:09:54 +0700

    fix bug k hien anh

--- src/modules/mapScreen/MapLayer/Contruction/StorageBuilding.js
+++ src/modules/mapScreen/MapLayer/Contruction/StorageBuilding.js
@@ -8,8 +8,13 @@ var StorageBuilding = Building.extend({
     presentImg: function() {
         var self = this;
         var present = (4 * this._curStorage / this._capacity - 0.5).toFixed(0);
-        if (present < 0) present = 0;
-        if (present >= 4) present = 3;
+        if (present < 0) {
+            present = 0;
+        } else if (present >= 4) {
+            present = 3;
+        } else {
+            present = 0;
+        }
         this.buildingImage.forEach(function(item, i) {
             if (i == present) self.buildingImage[i].opacity = 255;
             else self.buildingImage[i].opacity = 0;
