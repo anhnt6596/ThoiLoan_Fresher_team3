574ced89eab6aca7720fce1d668fb4edaf8ed3a5
duynd6 <linhrafa@gmail.com>
2018-09-07 16:50:03 +0700

    thay login screen

--- src/base/OptionButton.js
+++ src/base/OptionButton.js
@@ -3,6 +3,7 @@ var ui = ui || {};
 ui.optionButton = function(text, src1, src2, src3) {
     var button = ccui.Button(src1, src2, src3);
     var label = new cc.LabelBMFont(text, res.font_soji[16]);
+    button.label = label;
     label.attr({
         x: button.width / 2,
         y: button.height / 2,
